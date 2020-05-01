b05718f1dd3ba5a6bfa99929a353bca3be508046
Torbjorn Kringeland <tkringel@cisco.com>
2020-05-01 10:25:42 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Arduino Projects
+
+Various assortment of projects that come out of playing with my Arduino.
---
+++ dino-jump/main.ino
@@ -0,0 +1,18 @@
+int sensorValue;
+int sensorLow = 1023;
+int sensorHigh = 0;
+
+const int ledPin = 13;
+
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, HIGH);
+  
+  while (millis() < 5000) {
+  
+  
+  
+  
+  }
+}
